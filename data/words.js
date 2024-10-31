0f0f114450bcb8a6846a645ad94047870ebca6a5
lihongde <chinalihongde@gmail.com>
2024-10-31 21:31:07 +0800

    add words and upodate frontend libs

--- data/words.js
+++ data/words.js
@@ -1,7 +1,11 @@
 const words = [
   {
+    words: '人生到处知何似，应似飞鸿踏雪泥。泥上偶然留指爪，鸿飞那复计东西。',
+    author: '苏轼',
+  },
+  {
     words: '我们缺乏三种教育：性、爱、死亡教育。它们分别对应：身体完整、灵魂丰沛、生命价值',
-    author: 'unknown',
+    author: '佚名',
   },
 ]
 module.exports = words
