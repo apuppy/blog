d4d7687f3e0b54e34f59b87f007b95ea61e9a554
lihongde <chinalihongde@gmail.com>
2024-11-01 11:52:19 +0800

    add and update markdown docs

--- data/words.js
+++ data/words.js
@@ -7,5 +7,9 @@ const words = [
     words: '我们缺乏三种教育：性、爱、死亡教育。它们分别对应：身体完整、灵魂丰沛、生命价值',
     author: '佚名',
   },
+  {
+    words: '人总是在接近幸福时倍感幸福，在幸福进行时却患得患失',
+    author: '张爱玲',
+  },
 ]
 module.exports = words
